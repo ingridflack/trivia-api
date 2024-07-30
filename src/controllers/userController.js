51a8dde9ba3c1b0cf0f1472e4ae6588e7bdb0be9
ingridflack <ingrid_flack@hotmail.com>
2024-07-30 16:32:18 -0300

    Add login feature

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,13 @@
+import "dotenv/config";
 import { user } from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 class UserController {
   static async listUsers(_, res) {
     try {
-        const userList = await user.find({});
-        res.send(userList);
-
+      const userList = await user.find({});
+      res.send(userList);
     } catch (err) {
       res.status(500).json({ message: err.message });
     }
@@ -13,7 +15,28 @@ class UserController {
 
   static async registerUser(req, res) {
     try {
-      const newUser = await user.create(req.body);
+      const { password, passwordConfirmation } = req.body;
+
+      if (password !== passwordConfirmation) {
+        throw new Error("Passwords do not match");
+      }
+
+      const emailAlreadyExists = await user.findOne({ email: req.body.email });
+
+      if (emailAlreadyExists) {
+        throw new Error("E-mail already exists");
+      }
+
+      const salt = await bcrypt.genSalt(12);
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      const userData = {
+        ...req.body,
+        password: hashedPassword,
+      };
+
+      const newUser = await user.create(userData);
+
       res
         .status(201)
         .json({ message: "Successfully registered", user: newUser });
@@ -22,14 +45,49 @@ class UserController {
     }
   }
 
-  static async getUser(req, res) {
+  static async loginUser(req, res) {
     try {
-      const id = req.params.id;
-      const userData = await user.findById(id);
+      const { email, password } = req.body;
+
+      if (!email) {
+        throw new Error("Email are required");
+      }
+
+      if (!password) {
+        throw new Error("Password are required");
+      }
+
+      const userData = await user.findOne({ email: email });
 
       if (!userData) {
         throw new Error("User not found");
-      } 
+      }
+
+      const checkPassword = await bcrypt.compare(password, userData.password);
+
+      if (!checkPassword) {
+        throw new Error("Invalid password");
+      }
+
+      const secret = process.env.JWT_KEY;
+      const token = jwt.sign({ id: userData._id }, secret, {
+        expiresIn: "1h",
+      });
+
+      res.status(200).json({ message: "Successfully logged in", token });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  }
+
+  static async getUser(req, res) {
+    try {
+      const id = req.params.id;
+      const userData = await user.findById(id, "-password");
+
+      if (!userData) {
+        throw new Error("User not found");
+      }
 
       res.status(200).json(userData);
     } catch (err) {
@@ -49,14 +107,16 @@ class UserController {
         username,
         password,
       };
-      
+
       const updatedUser = await user.findByIdAndUpdate(id, body, { new: true });
 
       if (!updatedUser) {
         throw new Error("User not found");
       }
 
-      res.status(200).json({ message: "Successfully updated", user: updatedUser });
+      res
+        .status(200)
+        .json({ message: "Successfully updated", user: updatedUser });
     } catch (err) {
       res.status(500).json({ message: err.message });
     }
@@ -65,13 +125,15 @@ class UserController {
   static async deleteUser(req, res) {
     try {
       const id = req.params.id;
-     const deletedData = await user.findByIdAndDelete(id);
+      const deletedData = await user.findByIdAndDelete(id);
 
       if (!deletedData) {
         throw new Error("User not found");
-      } 
+      }
 
-      res.status(200).json({ message: "Successfully deleted", deleted: deletedData });
+      res
+        .status(200)
+        .json({ message: "Successfully deleted", deleted: deletedData });
     } catch (err) {
       res.status(500).json({ message: err.message });
     }
