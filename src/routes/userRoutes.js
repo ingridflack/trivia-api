83fc2b9048c429010efef311dfafe033b9ad3ca3
ingridflack <ingrid_flack@hotmail.com>
2024-08-09 19:01:26 -0300

    Remove param id from user routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,18 +1,12 @@
 import express from "express";
 import UserController from "../controllers/userController.js";
 import checkToken from "../middlewares/checkToken.js";
-import sameUserCheck from "../middlewares/sameUserCheck.js";
 
 const routes = express.Router();
 
-routes.get("/users", UserController.listUsers);
-routes.get("/users/:id", checkToken, sameUserCheck, UserController.getUser);
-routes.put("/users/:id", checkToken, sameUserCheck, UserController.updateUser);
-routes.delete(
-  "/users/:id",
-  checkToken,
-  sameUserCheck,
-  UserController.deleteUser
-);
+routes.get("/users/all", UserController.listUsers);
+routes.get("/users", checkToken, UserController.getUser);
+routes.put("/users", checkToken, UserController.updateUser);
+routes.delete("/users", checkToken, UserController.deleteUser);
 
 export default routes;
